afd2187df89bdef31498d9c39437aef8818c36ca
Bongkai <849789181@qq.com>
2020-05-14 23:39:13 +0800

    fix(createReducer): correcting returns warning

--- src/createReducer.ts
+++ src/createReducer.ts
@@ -12,7 +12,7 @@ export const createReducer = (
       const ret = mutation.operation && mutation.operation(draft)
       if (allowOperationReturns === true) {
         return ret
-      } else {
+      } else if (ret) {
         console.warn(
           `Returning value in mutation.operation is not allowed in '${mutation.type}', please mutate state directly or set 'allowOperationReturns' in storeConfig.`,
         )
